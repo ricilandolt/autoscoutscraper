f0dd3541a3fe9b94a87e3af11e7b8c1568f7f826
ricilandolt <ricardo.landolt1@gmail.com>
2024-03-30 20:01:08 +0100

    added prod startup script

--- startup.sh
+++ startup.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 sudo yum install -y git
 sudo yum install -y docker 
+sudo yum install -y amazon-cloudwatch-agent
 sudo systemctl start docker
 sudo chmod 666 /var/run/docker.sock
+sudo touch /var/log/scraper.log
+sudo chown -R ec2-user:ec2-user /var/log/scraper.log
 
-#sudo systemctl enable docker.service
-#sudo usermod -a -G docker ec2-user
-export CNX_STR=""
-export DB_NAME=""
-export COLL_NAME=""
-export COLL_NAME_LOG=""
-
-git clone https://github.com/ricilandolt/autoscoutscraper.git /home/ec2-user/autoscoutscraper
+git clone https://ricilandolt:<passoword>@github.com/ricilandolt/autoscoutscraper /home/ec2-user/autoscoutscraper
 sudo python3 -m venv /home/ec2-user/autoscoutscraper/virt
 source /home/ec2-user/autoscoutscraper/virt/bin/activate 
 sudo chown -R ec2-user:ec2-user /home/ec2-user/autoscoutscraper/virt
 pip install -r /home/ec2-user/autoscoutscraper/requirements.txt
-docker stop $(docker ps -a -q)
-docker run  -d -p 4444:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome
-cd /home/ec2-user/autoscoutscraper/
-sleep 30
-python /home/ec2-user/autoscoutscraper/car.py
+sudo chmod +x /home/ec2-user/autoscoutscraper/startscraper.sh
+sudo cp /home/ec2-user/autoscoutscraper/scraperservice.service /etc/systemd/system/scraperservice.service
+SCRIPTNAME="moto.py"
+SERVICE_FILE="/etc/systemd/system/scraperservice.service"
+PROXY_HOST = '' 
+PROXY_USER = '' 
+PROXY_PASS = '' 
+CNX_STR=""
+
+sed -i "/\[Service\]/a Environment=\"scriptname=$SCRIPTNAME\"" $SERVICE_FILE
+sed -i "/\[Service\]/a Environment=\"CNX_STR=$CNX_STR\"" $SERVICE_FILE
+sed -i "/\[Service\]/a Environment=\"PROXY_HOST=$PROXY_HOST\"" $SERVICE_FILE
+sed -i "/\[Service\]/a Environment=\"PROXY_USER=$PROXY_USER\"" $SERVICE_FILE
+sed -i "/\[Service\]/a Environment=\"PROXY_PASS=$PROXY_PASS\"" $SERVICE_FILE
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file://home/ec2-user/autoscoutscraper/cloudwatchconf.json -s
+
+systemctl daemon-reload
+systemctl enable scraperservice.service
+systemctl start scraperservice.service
