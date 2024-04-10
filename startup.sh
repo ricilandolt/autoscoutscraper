d216d1d49c95a5f4203d9e4a541edbc197bf0832
ricilandolt <ricardo.landolt1@gmail.com>
2024-04-10 16:05:38 +0200

    adjust startup

--- startup.sh
+++ startup.sh
@@ -7,24 +7,20 @@ sudo chmod 666 /var/run/docker.sock
 sudo touch /var/log/scraper.log
 sudo chown -R ec2-user:ec2-user /var/log/scraper.log
 
-git clone https://ricilandolt:<passoword>@github.com/ricilandolt/autoscoutscraper /home/ec2-user/autoscoutscraper
+git clone <repo> /home/ec2-user/autoscoutscraper
 sudo python3 -m venv /home/ec2-user/autoscoutscraper/virt
 source /home/ec2-user/autoscoutscraper/virt/bin/activate 
 sudo chown -R ec2-user:ec2-user /home/ec2-user/autoscoutscraper/virt
 pip install -r /home/ec2-user/autoscoutscraper/requirements.txt
 sudo chmod +x /home/ec2-user/autoscoutscraper/startscraper.sh
 sudo cp /home/ec2-user/autoscoutscraper/scraperservice.service /etc/systemd/system/scraperservice.service
-SCRIPTNAME="moto.py"
+SCRIPTNAME="car.py"
 SERVICE_FILE="/etc/systemd/system/scraperservice.service"
-PROXY_HOST = '' 
-PROXY_USER = '' 
-PROXY_PASS = '' 
+PROXY_PASS="" 
 CNX_STR=""
 
 sed -i "/\[Service\]/a Environment=\"scriptname=$SCRIPTNAME\"" $SERVICE_FILE
 sed -i "/\[Service\]/a Environment=\"CNX_STR=$CNX_STR\"" $SERVICE_FILE
-sed -i "/\[Service\]/a Environment=\"PROXY_HOST=$PROXY_HOST\"" $SERVICE_FILE
-sed -i "/\[Service\]/a Environment=\"PROXY_USER=$PROXY_USER\"" $SERVICE_FILE
 sed -i "/\[Service\]/a Environment=\"PROXY_PASS=$PROXY_PASS\"" $SERVICE_FILE
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file://home/ec2-user/autoscoutscraper/cloudwatchconf.json -s
 
